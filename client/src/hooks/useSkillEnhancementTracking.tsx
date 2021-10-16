75d2998e3ceaf8e6093cac087ce543f9b2a1f482
Diumbyte <diumbyte@gmail.com>
2021-10-15 21:32:33 -0400

    added local state tracking for skill enhancements

---
+++ client/src/hooks/useSkillEnhancementTracking.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react"
+import { Enhancement } from "../generated/graphql"
+import findLastIndex from "../util/findLastIndex"
+
+// TODO: Send unitId in here in order to initialize state from redux store in case it's being tracked already
+export const useSkillEnhancementTracking = (skillId: number, enhancements: Enhancement[]) => {
+
+    const [selectedCurrentEnhancementId, setSelectedCurrentEnhancementId] = useState(0)
+    const [selectedDesiredEnhancementId, setSelectedDesiredEnhancementId] = useState(0)
+    useEffect(() => {
+        if(enhancements !== undefined) {
+            setSelectedCurrentEnhancementId(0)
+            setSelectedDesiredEnhancementId(enhancements[0].id)
+        }
+    }, enhancements)
+
+    const onCurrentEnhancementClick = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedCurrentEnhancementId(parseInt(e.target.value))
+        const desiredEnhancement = enhancements.find(enh => enh.id === selectedDesiredEnhancementId) as Enhancement
+        const currentEnhancement = enhancements.find(enh => enh.id === parseInt(e.target.value)) as Enhancement
+
+        if(parseInt(e.target.value) === 0) {
+            return;
+        }
+        
+        
+                
+        if (desiredEnhancement.level <= currentEnhancement.level) {
+            const highestEnhancement = enhancements[enhancements.length - 1];
+            if(currentEnhancement.level === highestEnhancement.level) {
+                setSelectedDesiredEnhancementId(parseInt(e.target.value))
+            } else {
+                const oneLevelHigherThanCurrent = enhancements.find(enh => enh.level === currentEnhancement.level + 1) as Enhancement
+                setSelectedDesiredEnhancementId(oneLevelHigherThanCurrent.id)
+            }
+        }
+    }
+
+    const onDesiredEnhancementClick = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const currentEnhancement = enhancements.find(enh => enh.id === selectedCurrentEnhancementId) as Enhancement
+        const desiredEnhancement = enhancements.find(enh => enh.id === parseInt(e.target.value)) as Enhancement
+
+        if (selectedCurrentEnhancementId === 0 || desiredEnhancement.level > currentEnhancement.level) {
+            setSelectedDesiredEnhancementId(parseInt(e.target.value))
+        }
+    }
+    
+    return {
+        selectedCurrentEnhancementId,
+        selectedDesiredEnhancementId,
+        onCurrentEnhancementClick,
+        onDesiredEnhancementClick
+    }
+}
--- client/src/pages/components/SkillDetail.tsx
+++ client/src/pages/components/SkillDetail.tsx
@@ -1,5 +1,6 @@
 import { Enhancement } from "../../generated/graphql"
 import {ReactComponent as PlusIcon } from "../../assets/plus.svg"
+import { useSkillEnhancementTracking } from "../../hooks/useSkillEnhancementTracking"
 
 type SkillDetailProps = {
     id: number,
@@ -18,6 +19,12 @@ export const SkillDetail = ({
 }: SkillDetailProps) => {
 
     const currentEnhancementOptions = [{level: 0, id: 0}, ...enhancements.map(e => ({level: e.level, id: e.id}))];
+    const {
+        onCurrentEnhancementClick,
+        onDesiredEnhancementClick,
+        selectedCurrentEnhancementId,
+        selectedDesiredEnhancementId
+    } = useSkillEnhancementTracking(id, enhancements)
     
     return (
         <div className="row">
@@ -37,14 +44,26 @@ export const SkillDetail = ({
             {/* Current */}
             <div className="w-1/3 align-start">
                 <p className="text-xl">Current</p>
-                <select className="w-1/2 p-2 text-black" name={`startSkill${id}`} id={`startSkill${id}`}>
+                <select 
+                    className="w-1/2 p-2 text-black" 
+                    name={`startSkill${id}`} 
+                    id={`startSkill${id}`}
+                    value={selectedCurrentEnhancementId}
+                    onChange={onCurrentEnhancementClick}
+                >
                     {currentEnhancementOptions.map(e => <option key={e.id} value={e.id}>{e.level}</option>)}
                 </select>
             </div>
             {/* Goal */}
             <div className="w-1/3">
                 <p className="text-xl">Desired</p>
-                <select className="w-1/2 p-2 text-black" name={`desiredSkill${id}`} id={`desiredSkill${id}`}>
+                <select 
+                    className="w-1/2 p-2 text-black" 
+                    name={`desiredSkill${id}`} 
+                    id={`desiredSkill${id}`}
+                    value={selectedDesiredEnhancementId}
+                    onChange={onDesiredEnhancementClick}
+                >
                     {enhancements.map(e => <option key={e.id} value={e.id}>{e.level}</option>)}
                 </select>
             </div>
