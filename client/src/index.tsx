7e9489f0a49db1463ddc59432ddac682df94d75f
Diumbyte <diumbyte@gmail.com>
2021-11-27 23:19:53 -0500

    fixed uri bug

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,13 +6,13 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { InMemoryCache, ApolloClient, ApolloProvider, HttpLink } from '@apollo/client';
+import { InMemoryCache, ApolloClient, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router } from "react-router-dom"
 
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
-  link: new HttpLink({uri: "/api/test/woo"}),
+  uri: "/api",
   cache
 });
 
